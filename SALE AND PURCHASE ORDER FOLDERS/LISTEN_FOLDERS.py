cc92fc5dec4ef41da14131871a7237792b8d0303
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-03-29 16:46:43 -0400

    Refactored sigm_db_query to sql_query, accepts cursor as argument.

--- SALE AND PURCHASE ORDER FOLDERS/LISTEN_FOLDERS.py
+++ SALE AND PURCHASE ORDER FOLDERS/LISTEN_FOLDERS.py
@@ -14,7 +14,7 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 # Pull 'cli_id' record from 'order_header' table based on 'ord_no' record
 def all_ord_nos():
     sql_exp = f'SELECT ord_no FROM order_header'
-    result_set = sigm_db_query(sql_exp)
+    result_set = sql_query(sql_exp, sigm_db_cursor)
     ord_nos = tabular_data(result_set)
     return ord_nos
 
