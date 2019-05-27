118e7208291181cf3c925c57e98a9bdc038bbf9c
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-05-27 13:57:27 -0400

    Added illegal character handling

--- tasks.py
+++ tasks.py
@@ -7,14 +7,17 @@ def listen_task(config, notify):
 
     record_type, reference, user, station = data.payload_handler(raw_payload)
 
-    if record_type != 'PRT RENAME':
-        directory = files.create_dir(config, record_type, reference)
-        # Additional files/folders for parts
-        if record_type == 'PRT':
-            prt_no = reference
-            prt_folder = directory
-            files.extend_dir(config, prt_no, prt_folder)
+    if not any(i in reference for i in ('<', '>', ':', '"', '/', '\\', '|', '?', '*')):
+        if record_type != 'PRT RENAME':
+            directory = files.create_dir(config, record_type, reference)
+            # Additional files/folders for parts
+            if record_type == 'PRT':
+                prt_no = reference
+                prt_folder = directory
+                files.extend_dir(config, prt_no, prt_folder)
+        else:
+            old_prt_no = reference.split("}, {")[0][1:]
+            new_prt_no = reference.split("}, {")[1][:-1]
+            files.rename_prt_no(config, old_prt_no, new_prt_no)
     else:
-        old_prt_no = reference.split("}, {")[0][1:]
-        new_prt_no = reference.split("}, {")[1][:-1]
-        files.rename_prt_no(config, old_prt_no, new_prt_no)
+        print(f'{record_type} {reference} includes illegal character')
