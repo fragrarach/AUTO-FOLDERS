86c6fccf13c77f598d18907300fcd1de06791b0f
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2020-06-03 08:21:39 -0400

    Changed email recipients Removed config args

--- tasks.py
+++ tasks.py
@@ -11,20 +11,20 @@ def listen_task(config, notify):
     try:
         if not any(i in reference for i in ('<', '>', ':', '"', '/', '\\', '|', '?', '*')):
             if record_type not in ['PRT RENAME', 'CLI RENAME']:
-                directory = files.create_dir(config, record_type, reference)
+                directory = files.create_dir(record_type, reference)
                 # Additional files/folders for parts
                 if record_type == 'PRT':
                     prt_no = reference
                     prt_folder = directory
-                    files.extend_dir(config, prt_no, prt_folder)
+                    files.extend_dir(prt_no, prt_folder)
             elif record_type == 'PRT RENAME':
                 old_prt_no = reference.split("}, {")[0][1:]
                 new_prt_no = reference.split("}, {")[1][:-1]
-                files.rename_prt_no(config, old_prt_no, new_prt_no)
+                files.rename_prt_no(old_prt_no, new_prt_no)
             elif record_type == 'CLI RENAME':
                 old_cli_no = reference.split("}, {")[0][1:]
                 new_cli_no = reference.split("}, {")[1][:-1]
-                files.rename_cli_no(config, old_cli_no, new_cli_no)
+                files.rename_cli_no(old_cli_no, new_cli_no)
         else:
             log(f'{record_type} {reference} includes illegal character')
     except:
